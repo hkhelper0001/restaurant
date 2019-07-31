fef7266d2e17443cd9eccacb833181e8f73c8635
EC2 Default User <ec2-user@ip-172-31-33-106.ap-southeast-1.compute.internal>
2019-07-31 14:34:58 +0000

    test

---
+++ precache-manifest.a4d0e16f3bb0fe218409055c7dabb940.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831a1cc2c1e52603b25e377d760516b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "699231d62514b929e2d3",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "699231d62514b929e2d3",
+    "url": "./static/js/main.b1c3d48c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ precache-manifest.f283ca9da294f7b9df521e93783359ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fddc4a5ddd3e94eaf43080c0ae839994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee4fff46e9c7691a4cfb",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ee4fff46e9c7691a4cfb",
+    "url": "/static/js/main.f16acedd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
